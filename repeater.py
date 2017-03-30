2c38f40e14c28cd8557215f1f7f4696b4eb60d39
Jackie <tws00001@gmail.com>
2017-03-30 20:26:57 +0800

    jarvis try

--- repeater.py
+++ repeater.py
@@ -1,17 +1,28 @@
+# -*- coding: utf-8 -*-
+
 from gtts import gTTS
 from pygame import mixer
+mixer.init()
 import speech_recognition
 import tempfile as tp
 r= speech_recognition.Recognizer()
-with speech_recognition.Microphone() as source:
-    audio = r.listen(source)
-mixer.init()
 def google_repeater(say_again):
     with tp.NamedTemporaryFile(delete = True) as tfp: 
-        tts = gTTS(text=say_again, lang='zh')
+        tts = gTTS(text=say_again, lang='zh-tw')
         tts.save(tfp.name+".mp3")
         mixer.music.load(tfp.name+".mp3")
         mixer.music.play()
-
-myword=r.recognize_google(audio,language='zh-TW')
-google_repeater(myword)       
+# google_repeater("成功")
+try :
+    print "進入try"
+    with speech_recognition.Microphone() as source:
+        print "進入source"
+        audio = r.listen(source)
+    print "出來source"
+    myword=r.recognize_google(audio,language='zh-TW')
+    print "repeat之前"
+    google_repeater(myword)
+    print "try"
+except:
+    google_repeater("錄音失敗")
+    print "except"
---
+++ sound_test.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+from pygame import mixer
+mixer.init()
+mixer.music.load("/Users/Jackie/Desktop/python_course/audio.mp3")
+mixer.music.play()
